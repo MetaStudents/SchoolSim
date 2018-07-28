8e4c33b6348c8507d6a9997c35d3b239f46a825c
Isaac <isaacpd@yahoo.com>
2018-07-28 08:43:31 -0400

    Changed tabs to be consisitent with conventions

--- Source/Bar.hx
+++ Source/Bar.hx
@@ -23,71 +23,71 @@ class Bar extends Sprite
     // interestRate is the proportion of the total that increases per day
 
     public function new(interestRate:Float, workRate:Float, startHeight:Int, nameStr:String, lectures:Array<Lecture>, gameDate:Date, color:Int=0x000000) {
-	super();
-
-	movable = new Sprite();
-	movable.x = movable.y = 0;
-	column = new Shape();
-	column.graphics.lineStyle(0);
-	column.graphics.beginFill(color, 1);
-	column.graphics.drawRect(Math.round(barWidth/2),0,barWidth,1000);
-	column.graphics.endFill();
-	movable.addChild(column);
-
-	nameField = new TextField();
-	nameField.text = nameStr;
-	nameField.x = Math.round(barWidth/2);
-	nameField.y = -nameHeight;
-	movable.addChild(nameField);
-
-	addChild(movable);
-	movable.x = 0;
-	movable.y = -startHeight;
-	movable.addEventListener(MouseEvent.MOUSE_DOWN,
-				 function (e:MouseEvent)
-				 {working=true;});
-	movable.addEventListener(MouseEvent.MOUSE_UP,
-				 function (e:MouseEvent)
-				 {working=false;});
-	movable.addEventListener(MouseEvent.MOUSE_OUT,
-				 function (e:MouseEvent)
-				 {working=false;});
-
-	this.interestRate = interestRate;
-	this.workRate = workRate;
-	this.lectures = lectures;
-	lectureNum = 0;
-	initGameDate = gameDate;
-       	lectureEndDate = Util.jsonTimeToDate(initGameDate, lectures[lectureNum].times.split(" ")[1]);
+		super();
+		
+		movable = new Sprite();
+		movable.x = movable.y = 0;
+		column = new Shape();
+		column.graphics.lineStyle(0);
+		column.graphics.beginFill(color, 1);
+		column.graphics.drawRect(Math.round(barWidth/2),0,barWidth,1000);
+		column.graphics.endFill();
+		movable.addChild(column);
+		
+		nameField = new TextField();
+		nameField.text = nameStr;
+		nameField.x = Math.round(barWidth/2);
+		nameField.y = -nameHeight;
+		movable.addChild(nameField);
+		
+		addChild(movable);
+		movable.x = 0;
+		movable.y = -startHeight;
+		movable.addEventListener(MouseEvent.MOUSE_DOWN,
+					 function (e:MouseEvent)
+					 {working=true;});
+		movable.addEventListener(MouseEvent.MOUSE_UP,
+					 function (e:MouseEvent)
+					 {working=false;});
+		movable.addEventListener(MouseEvent.MOUSE_OUT,
+					 function (e:MouseEvent)
+					 {working=false;});
+		
+		this.interestRate = interestRate;
+		this.workRate = workRate;
+		this.lectures = lectures;
+		lectureNum = 0;
+		initGameDate = gameDate;
+		lectureEndDate = Util.jsonTimeToDate(initGameDate, lectures[lectureNum].times.split(" ")[1]);
     }
 
     public function update(gameDate:Date, delta:Float) {
-    	accrueInterest(delta);
-	if (working){
- 	    work(delta);
-	}
-	getHomework(gameDate);
+		accrueInterest(delta);
+		if (working){
+			work(delta);
+		}
+		getHomework(gameDate);
     }
 
     private function accrueInterest(delta:Float) {
-	//trace(interestRate+" "+delta/(3600*24)+" "+movable.y);
-	movable.y += interestRate*(delta/(3600*24))*movable.y; // make movable.y more negative
+		//trace(interestRate+" "+delta/(3600*24)+" "+movable.y);
+		movable.y += interestRate*(delta/(3600*24))*movable.y; // make movable.y more negative
     }
 
     private function work(delta:Float) {
-	//trace(workRate * delta/(3600*24));
+		//trace(workRate * delta/(3600*24));
         movable.y += workRate * delta/(3600*24);
     }
 
     private function getHomework(gameDate:Date){
-	if (lectureNum < lectures.length){
-	    if (gameDate.getTime() > lectureEndDate.getTime()){
-		movable.y -= lectures[lectureNum].size;
-		lectureNum++;
 		if (lectureNum < lectures.length){
-		    lectureEndDate = Util.jsonTimeToDate(initGameDate, lectures[lectureNum].times.split(" ")[1]);
+			if (gameDate.getTime() > lectureEndDate.getTime()){
+			movable.y -= lectures[lectureNum].size;
+			lectureNum++;
+				if (lectureNum < lectures.length){
+					lectureEndDate = Util.jsonTimeToDate(initGameDate, lectures[lectureNum].times.split(" ")[1]);
+				}
+			}
 		}
-	    }
-	}
     }
 }
