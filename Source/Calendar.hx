8c58b59a3e6313a2bbefbb0356006fb1a4914ef5
Isaac <isaacpd@yahoo.com>
2018-07-28 08:43:31 -0400

    Changed tabs to be consisitent with conventions

--- Source/Calendar.hx
+++ Source/Calendar.hx
@@ -19,95 +19,94 @@ class Calendar extends Sprite {
     private var endDate:Date; //first date not in calendar
     
     public function new(width, height, date:Date){
-	super();
-	today = Day.fromDate(date);
-	//round down to nearest Sunday to get endDate because nothing in calendar yet
-	endDate = DateTools.delta(date, -DateTools.days(date.getDay()));
-	
-	xOffset = 0;
-	yOffset = weekdayHeight;
-	cellWidth = Math.round(width/7);
-	cellHeight = Math.round((height-weekdayHeight)/2);
-	rows = 2;
-	
-	makeGrid();
-	makeWeekdays();
-	cells.get(today).backgroundColor = 0xEE5D15;
+		super();
+		today = Day.fromDate(date);
+		//round down to nearest Sunday to get endDate because nothing in calendar yet
+		endDate = DateTools.delta(date, -DateTools.days(date.getDay()));
+		
+		xOffset = 0;
+		yOffset = weekdayHeight;
+		cellWidth = Math.round(width/7);
+		cellHeight = Math.round((height-weekdayHeight)/2);
+		rows = 2;
+		
+		makeGrid();
+		makeWeekdays();
+		cells.get(today).backgroundColor = 0xEE5D15;
     }
 
     private function makeRow (rowNum){
-	for (j in 0...7){
-	    
-	    var cell:TextField = new TextField();
-	    addChild(cell);
-	    
-	    cell.x = xOffset + j*cellWidth;
-	    cell.y = yOffset + rowNum*cellHeight;
-	    cell.text = Std.string(endDate.getDate());
-	    trace(endDate);
-	    if (endDate.getDate()==1){
-		var ms:Array<String> = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-		var m = ms[endDate.getMonth()];
-		trace(ms);
-		trace(m);
-		cell.text += " "+m; 
-	    }
-	    
-	    cell.background = true;
-	    cell.backgroundColor = 0xFFFFFF;
-	    cell.border = true;
-	    cell.borderColor = 0x000000;
-	    cell.width = cellWidth;
-	    cell.height = cellHeight;
-
-	    cells.set(Day.fromDate(endDate),cell);
-		
-	    endDate = DateTools.delta(endDate, 24*3600*1000);//DateTools.days(1));
-	}
+		for (j in 0...7){
+			
+			var cell:TextField = new TextField();
+			addChild(cell);
+			
+			cell.x = xOffset + j*cellWidth;
+			cell.y = yOffset + rowNum*cellHeight;
+			cell.text = Std.string(endDate.getDate());
+			trace(endDate);
+			if (endDate.getDate()==1){
+				var ms:Array<String> = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+				var m = ms[endDate.getMonth()];
+				trace(ms);
+				trace(m);
+				cell.text += " "+m; 
+			}
+			
+			cell.background = true;
+			cell.backgroundColor = 0xFFFFFF;
+			cell.border = true;
+			cell.borderColor = 0x000000;
+			cell.width = cellWidth;
+			cell.height = cellHeight;
+			
+			cells.set(Day.fromDate(endDate),cell);
+			
+			endDate = DateTools.delta(endDate, 24*3600*1000);//DateTools.days(1));
+		}
     }
 
-    private function makeGrid()
-    {
-	for (i in 0...rows){
-	    makeRow(i);
-	}
+    private function makeGrid(){
+		for (i in 0...rows){
+			makeRow(i);
+		}
     }
 
     private function advanceGrid(){
-	for (cell in cells){
-	    if (cell.y <= yOffset){
-		removeChild(cell);
-	    } else {
-		cell.y -= cellHeight;
-	    }
-	}
-	makeRow(rows-1);
+		for (cell in cells){
+			if (cell.y <= yOffset){
+			removeChild(cell);
+			} else {
+			cell.y -= cellHeight;
+			}
+		}
+		makeRow(rows-1);
     }
 
     private function makeWeekdays() {
-	var names:Array<String> = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-	for (i in 0...7) {
-	    var weekday:TextField = new TextField();
-	    addChild(weekday);
-
-	    weekday.text = names[i];
-	    weekday.x = xOffset + i*cellWidth;
-	    weekday.y = yOffset - 15;
-	}
+		var names:Array<String> = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+		for (i in 0...7) {
+			var weekday:TextField = new TextField();
+			addChild(weekday);
+			
+			weekday.text = names[i];
+			weekday.x = xOffset + i*cellWidth;
+			weekday.y = yOffset - 15;
+		}
     }
 
     public function update(gameDate:Date) {
-	//trace("hi");
-	var day = Day.fromDate(gameDate);
-	//trace(today.month+" "+today.day);
-	//trace(day.month+" "+day.day);
-	if (!today.equals(day)){
-	    if (gameDate.getDay()==0){
-		advanceGrid();
-	    }
-	    cells.get(day).backgroundColor = 0xEE5D15;
-	    cells.get(today).backgroundColor = 0xFFFFFF;
-	    today=day;
-	}
+		//trace("hi");
+		var day = Day.fromDate(gameDate);
+		//trace(today.month+" "+today.day);
+		//trace(day.month+" "+day.day);
+		if (!today.equals(day)){
+			if (gameDate.getDay()==0){
+				advanceGrid();
+			}
+			cells.get(day).backgroundColor = 0xEE5D15;
+			cells.get(today).backgroundColor = 0xFFFFFF;
+			today=day;
+		}
     }
 }
